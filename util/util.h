fce89e2d15780a5cb905d6592367996833952f54
雷云飞 <yunfei_lei@126.com>
2015-07-14 17:47:19 +0800

    提取单词接口

--- util/util.h
+++ util/util.h
@@ -2,13 +2,14 @@
 #ifndef  _UTIL_H
 #define  _UTIL_H
 
-#include <iostream>
 #include <cstring>
 #include <cctype>
 #include <climits>
 #include <string>
 
-namespace util {
+using std::string;
+
+namespace base {
 
 // 剪除左端空格字符
 string ltrim(const string &str)
@@ -18,7 +19,7 @@ string ltrim(const string &str)
     {
         return str;
     }
-    return str:substr(pos);
+    return str.substr(pos);
 }
 
 // 剪除右端空格字符
@@ -29,7 +30,7 @@ string rtrim(const string &str)
     {
         return str;
     }
-    return str:substr(0, pos);
+    return str.substr(0, pos);
 }
 
 // 剪除左右两端空格字符
@@ -48,6 +49,47 @@ string trim(const string &str)
     }
 
     return str.substr(pos);
+}
+
+bool getword(const char *str, char *word, int &pos)
+{
+    if (str[pos] == 0)
+    {
+        return false;
+    }
+
+    const char *p1 = str + pos;
+    const char *p2 = p1;
+    
+    for (int i = 0; i <(int)strlen(p1) && !isalnum(p1[0]); i++)
+    {
+        ++p1;
+    }
+
+    p2 = p1;
+    
+    while (isalnum(p2[0]))
+    {
+        ++p2;
+    }
+
+    if (p2 == p1) 
+    {
+        return false;
+    }
+
+    int len = static_cast<int>(p2 - p1);
+    strncpy(word, p1, len);
+    word[len] = '\0';
+
+    for (int i = p2 - str; i < (int)strlen(str) && !isalnum(p2[0]); i++)
+    {
+        ++p2;
+    }
+
+    pos = static_cast<int>(p2 - str);
+
+    return true;
 }
 
 }
---
+++ util/util_test.cpp
@@ -0,0 +1,37 @@
+// =====================================================================================
+//
+//       Filename:  test.cpp
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2015年07月14日 16时43分13秒
+//       Revision:  none
+//       Compiler:  gcc
+//
+//         Author:  leiyunfei (yun63), towardstheway@gmail.com
+//   Organization:  
+//
+// =====================================================================================
+
+#include <iostream>
+#include "util.h"
+
+using namespace std;
+
+int main()
+{
+    const int kbuffersize = 255;
+    char buffer[kbuffersize + 1];
+    char word[kbuffersize + 1];
+    int pos = 0;
+
+    std::cin.getline(buffer, kbuffersize);
+
+    while (base::getword(buffer, word, pos))
+    {
+        cout << "Got this word: " << word << endl;
+    }
+
+    return 0;
+}
